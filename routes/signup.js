7086a82c20122c5275e4cfc5b2ddb016c451c22c
matthewhasan <matthewhasan1218@gmail.com>
2021-01-04 17:49:26 -0600

    added userValidators

--- routes/signup.js
+++ routes/signup.js
@@ -1,10 +1,45 @@
 var express = require('express');
 var router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
-const { check, validationResult } = require('express-validators');
+const { check, validationResult } = require('express-validator');
 const db = require('../db/models');
 
 //user validators
+const userValidators = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide an email.')
+        .isLength({ max: 100 })
+        .withMessage('Email must not exceed 100 characters.')
+        .isEmail()
+        .withMessage('Please provide a valid email address.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+        // Special Characters
+        //.matches(characters users must include)
+        //.withMessage('Password must contain ???')
+    check('confirmPassword')
+        .exists({ checkFalsy: true })
+        .withMessage('Please confirm your password.')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('These passwords do not match!');
+            }
+            return true;
+        })
+    ,
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for First Name')
+        .isLength({ max: 50 })
+        .withMessage('First Name must not be more than 50 characters long'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Last Name')
+        .isLength({ max: 50 })
+        .withMessage('Last Name must not be more than 50 characters long'),
+];
 
 
 router.get('/', csrfProtection, (req, res) => {
@@ -13,7 +48,7 @@ router.get('/', csrfProtection, (req, res) => {
     res.render('signup', { title: 'Sign Up', token: req.csrfToken() });
 });
 
-router.post('/', csrfProtection, asyncHandler(async(req, res) => {
+router.post('/', csrfProtection, userValidators, asyncHandler(async(req, res) => {
     //GOAL: Create new user into database.
     const { email, firstName, lastName, password } = req.body;
     const user = db.User.build({
