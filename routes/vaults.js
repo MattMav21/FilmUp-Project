f05418773ba127bd306b6102dbc66edaedf52a83
Lane <lanedaltonjohns@gmail.com>
2021-01-05 16:41:44 -0500

    complete function for a user to display their collections of vaults

--- routes/vaults.js
+++ routes/vaults.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/models');
 const { asyncHandler } = require('./utils');
+const { requireAuth } = require('../auth')
 
 // TODO: Ensure the middleware can check is the session/user is valid.
 // This middleware will check if a user is logged in. If so, nothing will happen.
@@ -14,18 +15,18 @@ const { asyncHandler } = require('./utils');
 //     next()
 // }
 
-router.get('/', asyncHandler(async (req, res) => {
+router.get('/', requireAuth, asyncHandler(async (req, res) => {
     // Since this page displays all of the vaults associated with the user
     // We want to display all vaults associated with their id
     // This id may need to be found on the session token we will implement
-    // const vaults = await db.Vault.findByPk(req.params.id)
+    const vaults = await db.Vault.findAll({ where: { userId: req.session.auth.userId } })
     // Need seed data to implement vaults on the page
     // We will then want to render the users vaults on the page
-    // res.render('vaults', { vaults })
-    res.render('vaults')
+    res.render('vaults', { vaults })
+    // res.render('vaults')
 }))
 
-router.get('/:id', asyncHandler(async (req, res) => {
+router.get('/:id', requireAuth, asyncHandler(async (req, res) => {
     // We want to access a specific vault
     // Likely will need to change this to match the specific vault
     // const vault = await db.Vault.findByPk(req.params.id)
@@ -34,7 +35,7 @@ router.get('/:id', asyncHandler(async (req, res) => {
     res.render('vault')
 }))
 
-router.post('/', asyncHandler(async (req, res) => {
+router.post('/', requireAuth, asyncHandler(async (req, res) => {
     // We want a route for when a user creates a vault
     // We will want to check how many vaults the user has. 
     // If more than ten, they can no longer create new vaults.
@@ -47,7 +48,7 @@ router.post('/', asyncHandler(async (req, res) => {
     await vault.save()
 }))
 
-router.delete('/:id', asyncHandler(async (req, res) => {
+router.delete('/:id', requireAuth, asyncHandler(async (req, res) => {
     // We want to target the specific vault
     const vault = await db.Vault.findByPk(req.params.id)
     // Remove the vault from the database
