59e554b444d7a25954995666f239277da7e59d0d
Lane <lanedaltonjohns@gmail.com>
2021-01-05 14:07:59 -0500

    add post route for creating a vault, add form, input, and button for the request on vaults pug page

--- routes/vaults.js
+++ routes/vaults.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const { check, validationResult } = require('express-validator');
 const db = require('../db/models');
 const { asyncHandler } = require('./utils');
 
@@ -19,7 +18,7 @@ router.get('/', asyncHandler(async (req, res) => {
     // Since this page displays all of the vaults associated with the user
     // We want to display all vaults associated with their id
     // This id may need to be found on the session token we will implement
-    // const vaults = await Vault.findByPk(req.params.id)
+    // const vaults = await db.Vault.findByPk(req.params.id)
     // Need seed data to implement vaults on the page
     // We will then want to render the users vaults on the page
     // res.render('vaults', { vaults })
@@ -29,9 +28,22 @@ router.get('/', asyncHandler(async (req, res) => {
 router.get('/:id', asyncHandler(async (req, res) => {
     // We want to access a specific vault
     // Likely will need to change this to match the specific vault
-    const vault = await Vault.findByPk(req.params.id)
+    // const vault = await db.Vault.findByPk(req.params.id)
     // Then render that specific vault on the page
-    res.render('vault', { vault })
+    // res.render('vault', { vault })
+    res.render('vault')
+}))
+
+router.post('/', asyncHandler(async (req, res) => {
+    // We want a route for when a user creates a vault
+    // We will want to check how many vaults the user has. 
+    // If more than ten, they can no longer create new vaults.
+    const user = await db.User.findByPk(req.params.id)
+    // TODO: Find a way to count how many vaults are associated with a user
+    const vault = await db.Vault.build({
+        name: req.name
+    })
+    await vault.save()
 }))
 
 module.exports = router;
