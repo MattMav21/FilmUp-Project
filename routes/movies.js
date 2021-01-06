90023b8c29dada31c6358d0848a1fc29f37c9dc7
Hero <heroroku@gmail.com>
2021-01-06 11:39:04 -0500

    added movie.css

--- routes/movies.js
+++ routes/movies.js
@@ -10,6 +10,13 @@ router.get('/', asyncHandler(async (req, res) => {
   res.render('movies', { movies })
 }));
 
+router.get(/\/\d+/, asyncHandler(async (req, res) => {
+  const id = req.path.split('/')[1]
+  const movie = await db.Movie.findByPk(id, { include: db.Genre })
+  console.log(movie)
+  res.render('movie', { movie })
+}))
+
 
 
 
