41975c241c966fe96d271f93e379e7706b19aed5
Guilherme Pace <pace.gui@gmail.com>
2021-01-05 18:20:54 -0600

    complete login/signup pug and start homepage

--- routes/signup.js
+++ routes/signup.js
@@ -32,8 +32,7 @@ const userValidators = [
                 throw new Error('These passwords do not match!');
             }
             return true;
-        })
-    ,
+        }),
     check('firstName')
         .exists({ checkFalsy: true })
         .withMessage('Please provide a value for First Name')
@@ -74,7 +73,12 @@ router.post('/', csrfProtection, userValidators, asyncHandler(async(req, res) =>
     } else {
 
         const errors = validatorErrors.array().map((error) => error.msg);
-        res.render('signup', { title: 'Sign Up', user, errors, token: req.csrfToken() })
+        res.render('signup', {
+            title: 'Sign Up',
+            user,
+            errors,
+            token: req.csrfToken()
+        });
     }
 }));
 
