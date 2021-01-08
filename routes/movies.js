e9b0b6b2f8c031897ef6769adb46900f906d4fb0
matthewhasan <matthewhasan1218@gmail.com>
2021-01-08 11:27:52 -0600

    cleaned up code

--- routes/movies.js
+++ routes/movies.js
@@ -12,8 +12,6 @@ router.get('/', asyncHandler(async (req, res) => {
 router.get(/\/\d+/, asyncHandler(async (req, res) => {
   const id = req.path.split('/')[1]
   const movie = await db.Movie.findByPk(id, { include: [db.Genre, {model: db.WatchedMovie, as: 'Reviews'}]})
-  // console.log(movie.toJSON())
-  //console.log(movie.Reviews)
   res.render('movie', { movie })
 }))
 
@@ -25,40 +23,25 @@ router.post('/:id/reviews', requireAuth, asyncHandler(async (req, res) => {
     content: req.body.content,
     movieId: movie.id,
   });
-  
-  // if (req.body.content !== "") {
-  // };
-  
+
   review.save();
   res.redirect(`/movies/${id}`)
 }));
 
-//
+
 
 router.post('/:id/reviews/:reviewId/edit', requireAuth, asyncHandler(async (req, res) => {
   const id = req.params.id;
   const movie = await db.Movie.findByPk(id, { include: db.Genre })
-  const reviewId = req.params.reviewId;
+  const { reviewId } = req.params;
   const review = await db.WatchedMovie.findByPk(reviewId);
-  console.log(reviewId)
-  // review.content = req.params;
-  // await review.save();
   res.render('movie-edit', {id, movie, reviewId, review})
 }));
 
 router.post('/:id/reviews/:reviewId/edit/edited', requireAuth, asyncHandler(async (req, res) => {
   const id = req.params.id;
-  //const movie = await db.Movie.findByPk(id, { include: [db.Genre, { model: db.WatchedMovie, as: 'Reviews' }] })
-  const reviewId = req.params.reviewId;
+  const { reviewId } = req.params;
   const review = await db.WatchedMovie.findByPk(reviewId);
-  // console.log('REQUEST PARAMETERS', req.params)
-  // console.log("ID", id)
-  // console.log("MOVIE", movie)
-  //console.log("REVIEW ID", reviewId)
-  //console.log("REVIEW CONTENT", review.content)
-  // console.log(req.body)
-  // console.log(req.params.id)
-  // console.log(review.content)
   review.content = req.body.content
   await review.save();
   res.redirect(`/movies/${id}`)
@@ -66,7 +49,7 @@ router.post('/:id/reviews/:reviewId/edit/edited', requireAuth, asyncHandler(asyn
 
 router.post('/:id/reviews/:reviewId/delete', requireAuth, asyncHandler(async (req, res) => {
   const id = req.params.id;
-  const reviewId = req.params.reviewId;
+  const { reviewId } = req.params;
   const review = await db.WatchedMovie.findByPk(reviewId);
   review.content = '';
   review.save();
