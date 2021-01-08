e4525d1017c57e92ba206d8aa1ef1ac90d3f15db
Lane <lanedaltonjohns@gmail.com>
2021-01-08 18:19:00 -0500

    add csrfProtection to vault routes

--- routes/vaults.js
+++ routes/vaults.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/models');
-const { asyncHandler } = require('./utils');
+const { asyncHandler, csrf, csrfProtection } = require('./utils');
 const { requireAuth } = require('../auth')
 
 
-router.get('/', requireAuth, asyncHandler(async (req, res) => {
+router.get('/', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
 
     // We want to display all vaults associated with user id
     const vaults = await db.Vault.findAll({ where: { userId: req.session.auth.userId } })
     // We will then want to render the users vaults on the page
-    res.render('vaults', { vaults })
+    res.render('vaults', { vaults, token: req.csrfToken() })
 }))
 
-router.get('/:id', requireAuth, asyncHandler(async (req, res) => {
+router.get('/:id', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
     // We want to access a specific vault
     const vault = await db.Vault.findByPk(req.params.id, { include: db.Movie })
     const user = await db.User.findOne({ where: { id: req.session.auth.userId } });
@@ -24,7 +24,7 @@ router.get('/:id', requireAuth, asyncHandler(async (req, res) => {
             vaultId: vault.id
         } }}
     })
-    res.render('vault', { vault, vaultedMovies })
+    res.render('vault', { vault, vaultedMovies, token:req.csrfToken() })
 }))
 
 router.delete('/:id', requireAuth, asyncHandler(async (req, res) => {
