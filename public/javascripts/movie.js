3e0abc361184988dfe2a02d5570cee57d5b4278f
Hero <heroroku@gmail.com>
2021-01-09 13:27:45 -0500

    cleaned up post routes in movies

--- public/javascripts/movie.js
+++ public/javascripts/movie.js
@@ -1,41 +1,29 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const watchedButton = document.getElementById('watched')
-  const movieIdInput = document.getElementById('movieId')
-  const span = document.getElementById('watchedSpan')
-  watchedButton.addEventListener('submit', async (event) => {
-    event.preventDefault()
-    // console.log('---------TEST------------')
-
-    const body = {
-      movieId: movieIdInput.value
-    }
-
-    if (span.innerHTML = 'Unwatched') {
-      const res = await fetch(`http//localhost:8080/movies/${movieIdInput.value}`, {
-
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body)
-      }).then(res => {
-        span.innerHTML = 'Watched'
-        res.json()
-      })
-    }
-  } else if (span.innerHTML = 'Watched') {
-    // console.log('TEST DELETE')
-    const res = await fetch(`http//localhost:8080/movies/${movieIdInput.value}`, {
-
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body)
-    }).then(res => {
-      span.innerHTML = 'Unwatched'
-      res.json()
-    })
-  }
-
-
-})
+  // const watchedButton = document.getElementById('watched')
+  // const movieIdInput = document.getElementById('movieId')
+  // const watchedSpan = document.getElementById('watched')
+  // watchedButton.addEventListener('click', async (event) => {
+  //   try {
+  //     const res = await fetch(`/movies/${movieIdInput.value}`, {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify({
+  //         movieId: movieIdInput.value
+  //       }),
+  //     })
+  //     if (!res.ok) {
+  //       throw res
+  //     }
+  //   } catch (err) {
+  //     console.error(err)
+  //   }
+  //   const res = await fetch(`http//localhost:8080/movies/${movieIdInput.value}`, {
+  //     method: 'DELETE',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify(body)
+  //   })
+  // }
+  // })
 
 
 
--- routes/movies.js
+++ routes/movies.js
@@ -13,22 +13,21 @@ router.get('/', asyncHandler(async (req, res) => {
 
 router.get('/genre/:id', csrfProtection, asyncHandler(async (req, res) => {
   const genreId = req.params.id;
-
   const movies = await db.Movie.findAll({
     include: db.Genre, where: { genreId }
   });
-
   console.log(movies[0]);
-
   res.render('movies', { movies, token: req.csrfToken() });
 }));
 
-router.get(/\/\d+/, csrfProtection, asyncHandler(async (req, res) => {
-  const id = req.path.split('/')[1]
+router.get('/:id', csrfProtection, asyncHandler(async (req, res) => {
+  const id = req.params.id
   const movie = await db.Movie.findByPk(id, { include: [db.Genre, { model: db.WatchedMovie, as: 'Reviews' }] })
+  const watched = await db.WatchedMovie.findOne({ where: { movieId: id, userId: req.session.auth.userId } })
+
   if (res.locals.authenticated) {
     const vaults = await db.Vault.findAll({ where: { userId: req.session.auth.userId } })
-    res.render('movie', { movie, vaults, token: req.csrfToken() })
+    res.render('movie', { movie, vaults, watched, token: req.csrfToken() })
   } else {
     res.render('movie', { movie })
   }
@@ -73,48 +72,33 @@ router.post('/:id/reviews/:reviewId/delete', requireAuth, csrfProtection, asyncH
   res.redirect(`/movies/${id}`);
 }));
 
-router.post('/:id', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
-  if (req.body.addToVault) {
-    const vaultedMovies = await db.VaultMovie.findAll({ where: { vaultId: req.body.vaultId, movieId: req.body.movieId } })
-    if (!vaultedMovies[0]) {
-      await db.VaultMovie.create({ vaultId: req.body.vaultId, movieId: req.body.movieId })
-      // res.json('added to vault')
-      res.redirect(`/movies/${req.body.movieId}`)
-    } else {
-      throw new Error('Movie already in vault')
-    }
-  } else if (req.body.watched) {
-    const watchedMovie = await db.WatchedMovie.findAll({ where: { userId: req.session.auth.userId, movieId: req.body.movieId } })
+router.post('/:id/watched', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
+  const watchedMovie = await db.WatchedMovie.findAll({ where: { userId: req.session.auth.userId, movieId: req.body.movieId } })
 
-    if (!watchedMovie[0]) {
-      const watchMovie = await db.WatchedMovie.create({ userId: req.session.auth.userId, movieId: req.body.movieId })
-      res.json(watchMovie)
-    }
-    // res.redirect(`/movies/${req.body.movieId}`)
-    // } else {
-    //   throw new Error('Movie already watched!')
-    // }
+  if (!watchedMovie[0]) {
+    await db.WatchedMovie.create({ userId: req.session.auth.userId, movieId: req.body.movieId })
+    res.redirect(`/movies/${req.body.movieId}`)
+  } else {
+    throw new Error('Movie already watched')
   }
+
 }))
 
-// router.delete(':id', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
+router.post('/:id/vaulted', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
+  const vaultedMovie = await db.VaultMovie.findAll({ where: { vaultId: req.session.auth.userId, movieId: req.body.movieId } })
 
-//   const watchedMovie = await db.watchedMovie.findOne({
-//     where: {
-//       movieId: req.body.movieId,
-//       usertId: req.session.auth.userId
-//     }
-//   })
-//   await watchedMovie.destroy()
-//   res.json()
+  if (!vaultedMovie[0]) {
+    await db.VaultMovie.create({ vaultId: req.body.vaultId, movieId: req.body.movieId })
+    res.redirect(`/movies/${req.body.movieId}`)
+  } else {
+    throw new Error('Movie already in vault!')
+  }
 
+}))
 
-// }))
 router.post("/search", csrfProtection, asyncHandler(async (req, res) => {
   const errors = ["We couldn't find any movies that match your search"];
-
   const searchTerm = `%${req.body.query}%`;
-
   if (req.body.query) {
     const movies = await db.Movie.findAll({
       where: {
@@ -123,11 +107,8 @@ router.post("/search", csrfProtection, asyncHandler(async (req, res) => {
         }
       }
     });
-
     if (!movies.length) return res.render('movies', { movies, errors, token: req.csrfToken() });
-
     res.render('movies', { movies, token: req.csrfToken() });
-
   } else {
     res.redirect("/");
   }
